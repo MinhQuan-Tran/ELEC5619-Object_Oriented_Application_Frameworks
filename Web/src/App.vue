4cda6949cc39fdbfe3c81512a1182eece18c4ffc
Minh Quan Tran <62203526+Someone112113@users.noreply.github.com>
2023-09-13 18:56:20 +1000

    Update

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,85 +1,41 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      taskName: "TaskBuddy"
+    }
+  },
+  computed: {
+    pageName() {
+      if (this.$route.name === "Task") {
+        return this.taskName;
+      }
+      return this.$route.name;
+    }
+  },
+  mounted() {
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Header v-if="!(pageName === 'Login')">{{ pageName }}</Header>
+  <router-view class="router-view" @pageName="(name: string) => taskName = name"></router-view>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.router-view {
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+@media (prefers-color-scheme: dark) {
+  .router-view {
+    background-color: var(--dark-background);
   }
 }
 </style>
