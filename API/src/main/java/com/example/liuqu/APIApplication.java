7e74506ffcf14392cf80ef7c0eb50bd55a8c825e
ego <43309561+zhangguifu123@users.noreply.github.com>
2023-09-25 23:40:45 +1000

    change name

--- API/src/main/java/com/example/liuqu/APIApplication.java
+++ API/src/main/java/com/example/liuqu/APIApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class LiuquApplication {
+public class APIApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(LiuquApplication.class, args);
