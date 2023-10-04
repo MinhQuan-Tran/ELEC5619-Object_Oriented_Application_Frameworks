d8d1c4006970b245991770d58a367ce41e5fe3df
ego <43309561+zhangguifu123@users.noreply.github.com>
2023-10-04 19:01:35 +1100

    change application name

--- API/src/main/java/com/example/liuqu/APIApplication.java
+++ API/src/main/java/com/example/liuqu/APIApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class APIApplication {
 	public static void main(String[] args) {
 
-		SpringApplication.run(LiuquApplication.class, args);
+		SpringApplication.run(APIApplication.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
