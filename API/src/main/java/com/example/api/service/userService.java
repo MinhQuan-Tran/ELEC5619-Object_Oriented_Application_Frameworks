7c8d09d1e26f135331c09ea7dbb1ef9866f8a913
ego <43309561+zhangguifu123@users.noreply.github.com>
2023-10-07 16:23:05 +1100

    fix liquibase

--- API/src/main/java/com/example/api/service/userService.java
+++ API/src/main/java/com/example/api/service/userService.java
@@ -14,20 +14,20 @@ public class userService {
     private UserRepository userRepository;
 
 /**
- * 注册
- * @param user 参数封装
+ * register
+ * @param user parameter encapsulation
  * @return Result
  */
     public Result register(User user) {
         Result<User> result = new Result<>();
         Optional<User> getUser = userRepository.findByEmail(user.getEmail());
         if (getUser.isPresent()) {
-            result.setResultFailed("该用户名已存在！");
+            result.setResultFailed("The username already exists!");
             return result;
         }
         user.setPassword(user.getPassword()); // TODO: Consider encrypting the password
         userRepository.save(user);
-        result.setResultSuccess("注册用户成功！", user);
+        result.setResultSuccess("Successfully registered user!", user);
         return result;
 
     }
@@ -36,11 +36,11 @@ public class userService {
         Result<String> result = new Result<>();
         User user = userRepository.findByEmail(emailOrPhone)
                 .orElse((User) userRepository.findByPhone(emailOrPhone).orElse(null));
-        if (user != null && user.getPassword().equals(password)) { // 实际应用中应使用加密密码
+        if (user != null && user.getPassword().equals(password)) { // Encrypted passwords should be used in practical applications
             result.setResultSuccess("login successful", user.getToken());
 
         }else{
-            result.setResultFailed("用户名和密码不匹配");
+            result.setResultFailed("User name and password do not match");
         };
         return result;
     }
@@ -48,7 +48,7 @@ public class userService {
     public void changePassword(Integer uid, String token, String newPassword) {
         User user = userRepository.findById(uid).orElse(null);
         if (user != null && user.getToken().equals(token)) {
-            user.setPassword(newPassword); // 实际应用中应使用加密密码
+            user.setPassword(newPassword); // Encrypted passwords should be used in practical applications
             userRepository.save(user);
         }
     }
