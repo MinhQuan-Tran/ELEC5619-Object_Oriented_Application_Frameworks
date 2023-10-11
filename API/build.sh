4f305814e270fad5fa42376ff8dd5e3cf2f5b581
ego <43309561+zhangguifu123@users.noreply.github.com>
2023-10-11 16:09:44 +1100

    login and register

--- API/build.sh
+++ API/build.sh
@@ -4,7 +4,7 @@
 # 构建docker镜像
 docker build -t springboot/springboot-5619:v1.0.0 ./docker/springboot
 # 运行镜像
-docker run --name=springboot-5619 -d --add-host host.docker.internal:host-gateway -p 8082:8080 springboot/springboot-5619:v1.0.0
+docker run --name=springboot-5619 -d --restart=always --add-host host.docker.internal:host-gateway -p 8082:8080 springboot/springboot-5619:v1.0.0
 # Run the sql file
 docker build -t liquibase/liquibase-mysql   ./docker/liquibase
 docker run --rm -it -v "$(pwd)/src/main/resources/db/changelog:/liquibase/changelog" liquibase/liquibase-mysql  update --defaultsFile=/liquibase/changelog/liquibase.docker.properties
--- API/src/main/java/com/example/api/service/userService.java
+++ API/src/main/java/com/example/api/service/userService.java
@@ -1,8 +1,11 @@
 package com.example.api.service;
 
+import com.example.api.dto.UserDTO;
 import com.example.api.model.Result;
 import com.example.api.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.api.repository.UserRepository;
 
@@ -13,31 +16,58 @@ public class userService {
     @Autowired
     private UserRepository userRepository;
 
-/**
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    /**
+     * *
+     * @param user
+     * @return userDTO
+     */
+    public UserDTO convertToDto(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUid(user.getUid());
+        userDTO.setNickname(user.getNickname());
+        userDTO.setName(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPhone(user.getPhone());
+        return userDTO;
+    }
+
+    /**
  * register
  * @param user parameter encapsulation
  * @return Result
  */
     public Result register(User user) {
-        Result<User> result = new Result<>();
+        Result<UserDTO> result = new Result<>();
         Optional<User> getUser = userRepository.findByEmail(user.getEmail());
         if (getUser.isPresent()) {
             result.setResultFailed("The username already exists!");
             return result;
         }
-        user.setPassword(user.getPassword()); // TODO: Consider encrypting the password
+        // encode password by Bcrypt
+        String plainPassword = user.getPassword();
+        String hashedPassword = passwordEncoder.encode(plainPassword);
+        user.setPassword(hashedPassword);
         userRepository.save(user);
-        result.setResultSuccess("Successfully registered user!", user);
+        UserDTO userDTO = convertToDto(user);
+        result.setResultSuccess("Successfully registered user!", userDTO);
         return result;
 
     }
 
-    public Result login(String emailOrPhone, String password) {
-        Result<String> result = new Result<>();
+    public Result login(String emailOrPhone, String plainPassword) {
+        Result<UserDTO> result = new Result<>();
+        // Encode login password
+        String hashedPassword = passwordEncoder.encode(plainPassword);
+//        print(hashedPassword);
+        // validate password
         User user = userRepository.findByEmail(emailOrPhone)
                 .orElse((User) userRepository.findByPhone(emailOrPhone).orElse(null));
-        if (user != null && user.getPassword().equals(password)) { // Encrypted passwords should be used in practical applications
-            result.setResultSuccess("login successful", user.getToken());
+        if (user != null && passwordEncoder.matches(plainPassword, user.getPassword())) { // Encrypted passwords should be used in practical applications
+            UserDTO userDTO = convertToDto(user);
+            result.setResultSuccess("login successful", userDTO);
 
         }else{
             result.setResultFailed("User name and password do not match");
@@ -48,6 +78,8 @@ public class userService {
     public void changePassword(Integer uid, String token, String newPassword) {
         User user = userRepository.findById(uid).orElse(null);
         if (user != null && user.getToken().equals(token)) {
+
+            String hashedPassword = passwordEncoder.encode(newPassword);
             user.setPassword(newPassword); // Encrypted passwords should be used in practical applications
             userRepository.save(user);
         }
