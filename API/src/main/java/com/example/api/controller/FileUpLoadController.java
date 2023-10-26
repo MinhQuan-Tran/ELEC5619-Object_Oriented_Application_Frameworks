67139126ffbf25a89916d5c13e59d35364c8af36
ego <43309561+zhangguifu123@users.noreply.github.com>
2023-10-27 05:53:33 +1100

    get image api

--- API/src/main/java/com/example/api/controller/FileUpLoadController.java
+++ API/src/main/java/com/example/api/controller/FileUpLoadController.java
@@ -1,16 +1,17 @@
 package com.example.api.controller;
 
 import com.example.api.model.Result;
-import jakarta.annotation.Resource;
-import lombok.Value;
+//import jakarta.annotation.Resource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -25,6 +26,51 @@ import java.util.Map;
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/api") // This means URL's start with /demo (after Application path)
 public class FileUpLoadController {
+
+    @Value("${app.base-url}")
+    private String baseUrl;
+
+    private String getFileExtension(String filename) {
+        int lastIndexOfDot = filename.lastIndexOf('.');
+        if (lastIndexOfDot == -1) {
+            return ""; // no extension found
+        }
+        return filename.substring(lastIndexOfDot + 1).toLowerCase();
+    }
+
+    private String determineContentType(String filename) {
+        String extension = getFileExtension(filename);
+        switch (extension) {
+            case "jpg":
+            case "jpeg":
+                return "image/jpeg";
+            case "png":
+                return "image/png";
+            case "gif":
+                return "image/gif";
+            default:
+                return "application/octet-stream";
+        }
+    }
+
+
+
+
+    
+    @GetMapping("/view/{filename}")
+    public ResponseEntity<Resource> viewFile(@PathVariable String filename) {
+        Path filePath = Paths.get("image/" + filename);
+        Resource resource = new FileSystemResource(filePath);
+        if (resource.exists()) {
+            String contentType = determineContentType(filename);
+            return ResponseEntity.ok()
+//                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 
     @PostMapping("/upload")
     public ResponseEntity<Result> handleFileUpload(@RequestParam("file") MultipartFile file) throws Exception {
@@ -50,7 +96,7 @@ public class FileUpLoadController {
         Path path = Paths.get("image/" + newFilename);
         Files.createDirectories(path.getParent());
         file.transferTo(path);
-        String fileUrl = "http:localhost:8082/image/" + newFilename;
+        String fileUrl =  baseUrl + "/api/view/" + newFilename;
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("imageURL",fileUrl);
         result.setResultSuccess(0,resultMap);
