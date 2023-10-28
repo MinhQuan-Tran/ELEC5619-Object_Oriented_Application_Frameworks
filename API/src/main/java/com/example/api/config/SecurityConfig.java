e734f052230edca18dd0a55c12cd18b40dec863c
ego <43309561+zhangguifu123@users.noreply.github.com>
2023-10-29 00:26:54 +1100

    cors

--- API/src/main/java/com/example/api/config/SecurityConfig.java
+++ API/src/main/java/com/example/api/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -22,15 +27,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())  // start CORS
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/hello").authenticated()
+                        .requestMatchers("/hello").authenticated()
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/image/**").permitAll()
                         .anyRequest().permitAll()
-
                 )
                 .csrf().disable();
         return http.build();
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
